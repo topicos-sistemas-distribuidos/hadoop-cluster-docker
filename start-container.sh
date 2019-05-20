84d901e89507d380917b24c10042056c30bccacb
Armando Soares Sousa <armando.sousa@gmail.com>
2019-05-20 17:20:35 -0300

    Update start-container.sh
    Atualizado para criar os containers baseados na imagem 1.1 com o Linux Ubuntu 16.04 e OpenJDK 8.

--- start-container.sh
+++ start-container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+  
 # the default node number is 3
 N=${1:-3}
 
@@ -13,22 +13,22 @@ sudo docker run -itd \
                 -p 8088:8088 \
                 --name hadoop-master \
                 --hostname hadoop-master \
-                kiwenlau/hadoop:1.0 &> /dev/null
+                kiwenlau/hadoop:1.1 &> /dev/null
 
 
 # start hadoop slave container
 i=1
 while [ $i -lt $N ]
 do
-	sudo docker rm -f hadoop-slave$i &> /dev/null
-	echo "start hadoop-slave$i container..."
-	sudo docker run -itd \
-	                --net=hadoop \
-	                --name hadoop-slave$i \
-	                --hostname hadoop-slave$i \
-	                kiwenlau/hadoop:1.0 &> /dev/null
-	i=$(( $i + 1 ))
-done 
+        sudo docker rm -f hadoop-slave$i &> /dev/null
+        echo "start hadoop-slave$i container..."
+        sudo docker run -itd \
+                        --net=hadoop \
+                        --name hadoop-slave$i \
+                        --hostname hadoop-slave$i \
+                        kiwenlau/hadoop:1.1 &> /dev/null
+        i=$(( $i + 1 ))
+done
 
 # get into hadoop master container
 sudo docker exec -it hadoop-master bash
